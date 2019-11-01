dc7fc6eeac894a3c4fc7bb13501c829cb28aea59
kaur0526 <54955041+kaur0526@users.noreply.github.com>
2019-10-31 22:36:06 -0400

    Update lab 3 exercise 10.py

--- lab 3 exercise 10.py
+++ lab 3 exercise 10.py
@@ -1,1 +1,4 @@
-
+miles-driven = float(input('enter the number of miles car driven'))
+gallons = float(input('enter the number of gallon used'))
+MPG = miles_driven / gallons
+print (" hence the MPG of car is = % 2.f" %MPG)
